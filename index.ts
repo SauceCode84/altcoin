44eff13639480fb7bc2a09847eaabb63fafac898
SauceCode84 <rayruok@gmail.com>
2018-04-14 18:59:24 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,75 @@
+import * as express from "express";
+import * as http from "http";
+import * as socketio from "socket.io";
+import { coinChangeFeed, fetchLiveStats, saveLiveStats, mapLiveStats } from "./livestats";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static("public"));
+
+server.listen(3000, () => console.log("Server listening on port 3000!"));
+
+const io = socketio.listen(server);
+
+const makeEmitUpdateCoin = (socket: socketio.Socket) => (value) => {
+  let { Coin, Price, Low, High, Volume } = value;
+
+  //let PricePrevHour = await coinPricePrevHour(Coin);
+  //let DiffHourPerc = (Price - PricePrevHour) / PricePrevHour * 100;
+  
+  socket.emit("coinUpdate", { Coin, Price, Low, High, Volume });
+}
+
+io.on("connection", async socket => {
+  console.log("Client connected...");
+
+  const emitUpdateCoin = makeEmitUpdateCoin(socket);
+
+  currentLiveStats.forEach(stat => emitUpdateCoin(stat));
+    
+  coins.forEach(async coin => {
+    let cursor = await coinChangeFeed(coin);
+    
+    cursor.eachAsync((row: any) => {
+      let { new_val } = row;
+      
+      emitUpdateCoin(new_val);
+    });
+  });
+});
+
+const timeout = 15000;
+let currentLiveStats;
+
+const startUp = async () => {
+  console.log("Starting up...");
+  
+  const fetchAndSaveLiveStats = async () => {
+    let liveStats = await fetchLiveStats();
+    await saveLiveStats(liveStats);
+    
+    currentLiveStats = mapLiveStats(liveStats);
+  }
+
+  await fetchAndSaveLiveStats();
+  setInterval(fetchAndSaveLiveStats, timeout);
+}
+
+startUp();
+
+const displayCoinFeed = (cursor) => {
+  cursor.each((err, row) => {
+    let { new_val } = row;
+    let { Coin, Price } = new_val;
+    
+    console.log(Coin + " Price:", Price);
+  });
+}
+
+const coins = ["BCH", "BTC", "BTG", "DASH", "ETH", "LTC", "NMC", "XRP", "ZEC"];
+
+coins.forEach(async coin => {
+  let cursor = await coinChangeFeed(coin);
+  displayCoinFeed(cursor);
+});
---
+++ livestats.ts
@@ -0,0 +1,79 @@
+import fetch from "node-fetch";
+import * as r from "rethinkdb";
+
+const apiUrl = "https://www.altcointrader.co.za/api/v3/live-stats";
+const connectionConfig = {
+    host: "localhost",
+    port: 28015,
+    db: "altcoin"
+};
+
+type CoinTypes = "BCH" | "BTC" | "BTG" | "DASH" | "ETH" | "LTC" | "NMC" | "XRP" | "ZEC";
+
+type LiveStatsResponse = {
+  [coin in CoinTypes]: {
+    Price: string;
+    Sell: string;
+    Buy: string;
+    High: string;
+    Low: string;
+    Volume: string;
+  }
+};
+
+export const fetchLiveStats = async () => {
+  try {
+    let response = await fetch(apiUrl);
+    return response.json<LiveStatsResponse>();
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+
+const getConnection = () => r.connect(connectionConfig);
+
+export const mapLiveStats = (liveStats: LiveStatsResponse) => {
+  return Object.keys(liveStats)
+    .map(key => {
+      liveStats[key].Coin = key;
+      liveStats[key].Timestamp = new Date();
+      
+      return liveStats[key];
+    });
+}
+
+export const saveLiveStats = async (liveStats) => {
+  let values = mapLiveStats(liveStats);
+  let connection = await getConnection();
+    
+  await r.table("livestats")
+    .insert(values)
+    .run(connection);
+    
+  console.log("inserted liveStats");
+}
+
+export const coinChangeFeed = async (coin) => {
+  let connection = await getConnection();
+  
+  return await r.table("livestats")
+    .filter({ Coin: coin })
+    .changes()
+    .run(connection);
+}
+
+export const coinPricePrevHour = async (coin) => {
+  let connection = await getConnection();
+  let cursor = await r.table("livestats")
+    .filter({ Coin: coin })
+    .orderBy("Timestamp")
+    .filter(liveStat => liveStat("Timestamp").ge(r.now().sub(60 * 60)))
+    .limit(1)
+    .pluck("Price")
+    .run(connection);
+  
+  let [ row ] = await cursor.toArray();
+  
+  return row.Price;
+}
